60b7a28b8fef9edd350b2c3cbc79a3a6d50376d6
topvila <topvila@hotmail.com>
2019-05-15 14:53:05 -0300

    serve construido e rodando

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //coments
+
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const HOST = '0.0.0.0';
+
+app.get('/',function(req,res){
+	res.send('serve funcionando :)');
+})
+app.listen(PORT,HOST);
